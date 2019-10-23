5a25c874154b4211352ebf5e3b399666f7e8107b
migu_el_39@hotmail.com <migu_el_39@hotmail.com>
2019-10-22 22:09:57 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TrabajadorComponent } from './pages/trabajador/trabajador.component';
+import { TrabajadoresComponent } from './pages/trabajadores/trabajadores.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrabajadorComponent,
+    TrabajadoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
